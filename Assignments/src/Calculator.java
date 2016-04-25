8e572d60d0956b95a614580cec286010a07ff61a
Vikram <Vikram@Vikram-PC>
2016-04-25 15:18:49 -0700

    Latest update

--- Assignments/src/Calculator.java
+++ Assignments/src/Calculator.java
@@ -1,7 +1,30 @@
 
 public class Calculator {
+	/**
+	 * This is the method just prints calculator project.
+	 * @param args
+	 */
 	public static void main(String args[]){
-		System.out.println("This is calculator project");
+		System.out.println("This is calculator project" +args[0]+"  "+args[1] );
+		printMe();
+		//control + space
+		System.out.println(returnYourName());
+		System.out.println(System.currentTimeMillis());
+		System.out.println();//Sysout + ctrl + space
+		System.out.println("Calculator.main()");//Systrace + ctrl + space
+		
+	}
+	/*
+	 * This is test method
+	 */
+	public static void printMe()
+	{
+		System.out.println("PrintMe" +"  "+returnYourName());
+	}
+	public  static String  returnYourName()
+	{
+		return "Nirmala";
+		
 	}
 
 }
